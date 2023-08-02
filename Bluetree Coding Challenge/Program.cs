b38395ee5df537832f5d462d5a8424e6194ee300
scott <scott@DESKTOP-5D4P18T>
2023-08-02 17:19:44 +0100

    Add project files.

---
+++ Bluetree Coding Challenge/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace Bluetree_Coding_Challenge
+{
+    class ItemScanner
+    {
+        public void Run()
+        {
+            bool appOn = true;
+            Console.WriteLine("Item scanner running. Type \"stop\" to stop the program");
+            int[] scannedItems = new int[4];
+            do
+            {
+                Console.WriteLine("Scan an item: ");
+                string itemSku = Console.ReadLine();
+                string lowerCaseSku = itemSku;
+                // SKUs are usually case sensitive so the user will have to put in case sensitive skus, but the stop command does not need to be
+                if (lowerCaseSku != null) { lowerCaseSku = lowerCaseSku.ToLower(); }
+
+                if (itemSku == "A")
+                {
+                    scannedItems[0]++;
+                }
+                else if (itemSku == "B")
+                {
+                    scannedItems[1]++;
+                }
+                else if (itemSku == "C")
+                {
+                    scannedItems[2]++;
+                }
+                else if (itemSku == "D")
+                {
+                    scannedItems[3]++;
+                }
+                else if (lowerCaseSku == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid SKU");
+                    continue;
+                }
+
+                int total = CalculateTotal(scannedItems);
+
+                Console.WriteLine($"Current total is: {total}");
+            } while (appOn);
+
+            int finalTotal = CalculateTotal(scannedItems);
+            Console.WriteLine($"Final total is: {finalTotal}");
+        }
+
+        static void Main()
+        {
+            var itemScanner = new ItemScanner();
+
+            itemScanner.Run();
+        }
+
+        internal static int CalculateTotal(int[] scannedItems)
+        {
+            int total = 0;
+
+            total += CalculateTotalCost(scannedItems[0], "A");
+            total += CalculateTotalCost(scannedItems[1], "B");
+            total += CalculateTotalCost(scannedItems[2], "C");
+            total += CalculateTotalCost(scannedItems[3], "D");
+
+            return total;
+        }
+
+        static int CalculateTotalCost(int amount, string sku)
+        {
+            int divideBy = 1;
+            int unitPrice;
+            int salePrice = 0;
+
+            if (sku == "A")
+            {
+                divideBy = 3;
+                unitPrice = 50;
+                salePrice = 130;
+            }
+            else if (sku == "B")
+            {
+                divideBy = 2;
+                unitPrice = 30;
+                salePrice = 45;
+            }
+            else if (sku == "C")
+            {
+                unitPrice = 20;
+            }
+            else if (sku == "D")
+            {
+                unitPrice = 15;
+            }
+            else
+            {
+                throw new Exception("Invalid SKU");
+            }
+
+            int result;
+            // Divide amount by divideBy
+            if (salePrice != 0)
+            {
+                int divideAndRoundDown = (int)Math.Floor((double)amount / divideBy);
+                int saleItemsValue = divideAndRoundDown *= salePrice;
+                int remainder = amount % divideBy;
+                if (remainder != 0)
+                {
+                    int nonSaleItems = remainder * unitPrice;
+                    result = saleItemsValue + nonSaleItems;
+                }
+                else
+                {
+                    result = saleItemsValue;
+                }
+            }
+            else
+            {
+                result = amount * unitPrice;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Bluetree Coding Challenge/ProgramTests.cs
@@ -0,0 +1,106 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace Bluetree_Coding_Challenge.Tests
+{ 
+    [TestFixture]
+    public class ProgramTests
+    {
+        private StringWriter? consoleOutput;
+
+        [SetUp]
+        public void Setup()
+        {
+            consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            consoleOutput?.Dispose();
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+        }
+
+        private int[] GetScannedItemsArray(int aCount, int bCount, int cCount, int dCount, int otherCount)
+        {
+            int[] scannedItems = new int[5];
+            scannedItems[0] = aCount;
+            scannedItems[1] = bCount;
+            scannedItems[2] = cCount;
+            scannedItems[3] = dCount;
+            scannedItems[4] = otherCount;
+            return scannedItems;
+        }
+
+        [Test]
+        public void TestCalculateTotals()
+        {
+            int[] scannedItems1 = GetScannedItemsArray(2, 3, 1, 4, 0);
+            int expectedTotal1 = 255;
+            int actualTotal1 = ItemScanner.CalculateTotal(scannedItems1);
+            Assert.AreEqual(expectedTotal1, actualTotal1);
+
+            int[] scannedItems2 = GetScannedItemsArray(1, 2, 0, 1, 0);
+            int expectedTotal2 = 110;
+            int actualTotal2 = ItemScanner.CalculateTotal(scannedItems2);
+            Assert.AreEqual(expectedTotal2, actualTotal2);
+
+            int[] scannedItems3 = GetScannedItemsArray(0, 0, 0, 0, 0);
+            int expectedTotal3 = 0;
+            int actualTotal3 = ItemScanner.CalculateTotal(scannedItems3);
+            Assert.AreEqual(expectedTotal3, actualTotal3);
+
+            int[] scannedItems4 = GetScannedItemsArray(6, 1, 2, 3, 0);
+            int expcetedTotal4 = 375;
+            int actualTotal4 = ItemScanner.CalculateTotal(scannedItems4);
+            Assert.AreEqual(expcetedTotal4, actualTotal4);
+
+            int[] scannedItems5 = GetScannedItemsArray(2, 4, 1, 1, 2);
+            int expectedTotal5 = 225;
+            int actualTotal5 = ItemScanner.CalculateTotal(scannedItems5);
+            Assert.AreEqual(expectedTotal5, actualTotal5);
+        }
+
+        [Test]
+        public void TestItemScannerWithValidInputs()
+        {
+            var inputReader = new StringReader("A\nB\nC\nD\nstop\n");
+            var outputWriter = new StringWriter();
+
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+
+            var itemScanner = new ItemScanner();
+
+            itemScanner.Run();
+
+            string actualOutput = outputWriter.ToString();
+            StringAssert.Contains("Item scanner running. Type \"stop\" to stop the program", actualOutput);
+            StringAssert.Contains("Scan an item: ", actualOutput);
+            StringAssert.Contains("Current total is: ", actualOutput);
+            StringAssert.Contains("Final total is: ", actualOutput);
+
+            inputReader.Dispose();
+            outputWriter.Dispose();
+        }
+
+        [Test]
+        public void TestItemScannerWithInvalidInputs()
+        {
+            var inputReader = new StringReader("X\nstop\n");
+            var outputWriter = new StringWriter();
+
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+
+            var itemScanner = new ItemScanner();
+
+            itemScanner.Run();
+
+            string actualOutput = outputWriter.ToString();
+            StringAssert.Contains("Invalid SKU", actualOutput);
+        }
+    }
+}
